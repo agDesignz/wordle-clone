e286b018d0c0bde4c4bb93601dd97fa9f8d5f697
mnartstuff <68624179+mnartstuff@users.noreply.github.com>
2022-04-13 17:33:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,82 +1,161 @@
-( () => {
+(() => {
 
-"use strict";
-
-const wordle = "MARIO";
+  "use strict";
 
   const mainSection = document.querySelector("#main-section");
-for (let rc = 0; rc <= 5; rc++) {
-  const newRow = document.createElement("div");
-  newRow.className = "wordle-row";
-  newRow.id = "row-" + rc;
-  for (let cc = 0; cc <= 4; cc++) {
-    const newBox = document.createElement("div");
-    newBox.className = "wordle-box";
-    newBox.id = `r${rc}seat${cc}`;
-    newRow.appendChild(newBox);
+  for (let rc = 0; rc <= 5; rc++) {
+    const newRow = document.createElement("div");
+    newRow.className = "wordle-row";
+    newRow.id = "row-" + rc;
+    for (let cc = 0; cc <= 4; cc++) {
+      const newBox = document.createElement("div");
+      newBox.className = "wordle-box";
+      newBox.id = `row${rc}box${cc}`;
+      newRow.appendChild(newBox);
+    }
+    mainSection.appendChild(newRow);
   }
-  mainSection.appendChild(newRow);
-}
 
-const rows = document.querySelectorAll("wordle-row");
-let rowCount = 0;
-
-function guess() {
-  const thisGuess = [];
-  const boxes = document.querySelectorAll(`#row-${rowCount} > .wordle-box`);
+  const rows = document.querySelectorAll("wordle-row");
   const buttons = document.querySelectorAll(".keyboard-button");
-  buttons.forEach( button => {
+
+  const wordleWord = "MARIO";
+  const totalTries = 6;
+  let remainingTries = totalTries;
+  let thisTry = [];
+  let nextLetter = 0;
+
+  buttons.forEach(button => {
     button.addEventListener("click", (e) => {
-      const letter = e.target.innerHTML.toUpperCase();
-      if(letter == "DEL") {
-        // DEAL WITH THE DELETE AND ENTER BUTTONS
-        thisGuess.pop();
-      } else if (letter == "ENTER"){
-        // THE BIG REVEAL!!!
-        const myWord = thisGuess.join("");
-        finalGuess(myWord);
+      if (remainingTries === 0) {
+        return;
+      }
+      let keyTap = e.target.innerText;
+      if (keyTap === "DEL") {
+        deleteOne();
+        return;
+      } else if (keyTap === "ENTER") {
+        verify();
         return;
       } else {
-        if (thisGuess.length < wordle.length) {
-          thisGuess.push(letter);
+        if (thisTry.length >= 5) {
+          return;
         }
+        placeLetter(keyTap);
       }
-      for(let i = 0; i < boxes.length; i++){
-        if (thisGuess[i]) {
-          boxes[i].innerHTML = `<h3>${thisGuess[i]}`;
-        } else {
-          boxes[i].innerHTML = "";
-        }
-      }
-
     });
   });
 
-
-}
-
-
-  /////////////////  finalGuess funtion
-  function finalGuess(myGuess){
-    const boxes = document.querySelectorAll(`#row-${rowCount} > .wordle-box`);
-    for (let i = 0; i < myGuess.length; i++) {
-      if (myGuess[i] === wordle[i]) {
-        boxes[i].classList.add("spot-on");
-      } else if (wordle.includes(myGuess[i])) {
-        boxes[i].classList.add("almost");
-      } else {
-        boxes[i].classList.add("wrong");
-      }
+  function placeLetter(entry) {
+    if (nextLetter > 4) {
+      return;
     }
-    rowCount ++;
-    guess();
+    let row = totalTries - remainingTries;
+    let box = nextLetter;
+    document.querySelector(`#row${row}box${box}`).innerText = entry;
+    thisTry.push(entry);
+    nextLetter++;
   }
 
+  function deleteOne() {
+    if (nextLetter <= 0) {
+      return;
+    }
+    let row = totalTries - remainingTries;
+    let box = nextLetter - 1;
+    document.querySelector(`#row${row}box${box}`).innerText = "";
+    nextLetter--;
+    thisTry.pop();
+  }
 
-///////////////////   END OF finalGuess function
+  function verify() {
+    checkWord();
+    let answer = Array.from(wordleWord);
+    if (thisTry.length < 5) {
+      tooShort();
+      return;
+    } else { //  VERIFY MAIN PART START
+      let row = totalTries - remainingTries
+      for (let place = 0; place < thisTry.length; place++) {
+        let box = document.querySelector(`#row${row}box${place}`);
+        let boxText = box.textContent;
+        let resultClass;
+        if (thisTry[place] === answer[place]) {
+          resultClass = "spot-on";
+        } else if (answer.includes(thisTry[place])) {
+          resultClass = "almost";
+        } else {
+          resultClass = "wrong";
+        }
 
-guess();
+          let delay = 500 * place;
+          setTimeout(() => {
+            box.classList.add(resultClass);
+            for (const keyButton of document.querySelectorAll(".keyboard-button")) {
+              if (keyButton.innerText === boxText) {
+                keyButton.classList.add(resultClass);
+                break;
+              }
+            }
+          }, delay);
 
+      } // end for-loop
+    } // VERIFY MAIN PART END
+    nextTry();
 
+      function nextTry() {
+        if (
+          thisTry.every((val, index) => val === answer[index])
+        ) {
+          youWin();
+          return;
+        } else if (remainingTries === 1) {
+          gameOver();
+        } else {
+          remainingTries--;
+          console.log(`${remainingTries} tries are left!`);
+          thisTry = [];
+          nextLetter = 0;
+        }
+      }
+  } // VERIFY END
+
+  function checkWord() {
+    const theWord = thisTry.join("").toLocaleLowerCase();
+    const wordResult = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${theWord}`);
+      wordResult.then( response => {
+        if (response) {
+          console.log(response.json());
+        } else {
+          console.log(`no response`);
+        }
+      });
+  }
+
+  const modal = document.querySelector(".modal");
+  const shortModal = document.createElement("div");
+  shortModal.className = "shortModal";
+  shortModal.innerText = "Too Short";
+  const closer = document.createElement("button");
+  closer.classList.add("keyboard-button", "short-closer");
+  shortModal.appendChild(closer);
+  closer.innerText = "OK";
+
+  function tooShort() {
+    modal.classList.add("flex");
+    modal.appendChild(shortModal);
+    closer.addEventListener("click", () => {
+      modal.removeChild(shortModal);
+      modal.classList.remove("flex");
+    });
+  }
+
+  function youWin() {
+    modal.classList.add("flex");
+  }
+
+  function gameOver() {
+    console.log("game over")
+  }
 
 })();
