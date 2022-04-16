286f6faa85234171ae0e63b1ba4545db242655c3
mnartstuff <68624179+mnartstuff@users.noreply.github.com>
2022-04-16 15:56:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,161 +1,193 @@
 (() => {
 
-  "use strict";
+    "use strict";
 
-  const mainSection = document.querySelector("#main-section");
-  for (let rc = 0; rc <= 5; rc++) {
-    const newRow = document.createElement("div");
-    newRow.className = "wordle-row";
-    newRow.id = "row-" + rc;
-    for (let cc = 0; cc <= 4; cc++) {
-      const newBox = document.createElement("div");
-      newBox.className = "wordle-box";
-      newBox.id = `row${rc}box${cc}`;
-      newRow.appendChild(newBox);
-    }
-    mainSection.appendChild(newRow);
-  }
-
-  const rows = document.querySelectorAll("wordle-row");
-  const buttons = document.querySelectorAll(".keyboard-button");
-
-  const wordleWord = "MARIO";
-  const totalTries = 6;
-  let remainingTries = totalTries;
-  let thisTry = [];
-  let nextLetter = 0;
-
-  buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-      if (remainingTries === 0) {
-        return;
+    const mainSection = document.querySelector("#main-section");
+    for (let rc = 0; rc <= 5; rc++) {
+      const newRow = document.createElement("div");
+      newRow.className = "wordle-row";
+      newRow.id = "row-" + rc;
+      for (let cc = 0; cc <= 4; cc++) {
+        const newBox = document.createElement("div");
+        newBox.className = "wordle-box";
+        newBox.id = `row${rc}box${cc}`;
+        newRow.appendChild(newBox);
       }
-      let keyTap = e.target.innerText;
-      if (keyTap === "DEL") {
-        deleteOne();
-        return;
-      } else if (keyTap === "ENTER") {
-        verify();
-        return;
-      } else {
-        if (thisTry.length >= 5) {
-          return;
-        }
-        placeLetter(keyTap);
-      }
-    });
-  });
-
-  function placeLetter(entry) {
-    if (nextLetter > 4) {
-      return;
+      mainSection.appendChild(newRow);
     }
-    let row = totalTries - remainingTries;
-    let box = nextLetter;
-    document.querySelector(`#row${row}box${box}`).innerText = entry;
-    thisTry.push(entry);
-    nextLetter++;
-  }
 
-  function deleteOne() {
-    if (nextLetter <= 0) {
-      return;
-    }
-    let row = totalTries - remainingTries;
-    let box = nextLetter - 1;
-    document.querySelector(`#row${row}box${box}`).innerText = "";
-    nextLetter--;
-    thisTry.pop();
-  }
+    const rows = document.querySelectorAll("wordle-row");
+    const buttons = document.querySelectorAll(".keyboard-button");
+    const modal = document.querySelector(".modal");
 
-  function verify() {
-    checkWord();
-    let answer = Array.from(wordleWord);
-    if (thisTry.length < 5) {
-      tooShort();
-      return;
-    } else { //  VERIFY MAIN PART START
-      let row = totalTries - remainingTries
-      for (let place = 0; place < thisTry.length; place++) {
-        let box = document.querySelector(`#row${row}box${place}`);
-        let boxText = box.textContent;
-        let resultClass;
-        if (thisTry[place] === answer[place]) {
-          resultClass = "spot-on";
-        } else if (answer.includes(thisTry[place])) {
-          resultClass = "almost";
-        } else {
-          resultClass = "wrong";
-        }
 
-          let delay = 500 * place;
-          setTimeout(() => {
-            box.classList.add(resultClass);
-            for (const keyButton of document.querySelectorAll(".keyboard-button")) {
-              if (keyButton.innerText === boxText) {
-                keyButton.classList.add(resultClass);
-                break;
-              }
+    const wordleWord = "PLACE";
+    const totalTries = 6;
+    let remainingTries = totalTries;
+    let thisTry = [];
+    let nextLetter = 0;
+
+    function guessWord() {
+
+      buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+          if (remainingTries === 0) {
+            return;
+          }
+          let keyTap = e.target.innerText;
+          if (keyTap === "DEL") {
+            deleteOne();
+            return;
+          } else if (keyTap === "ENTER") {
+            if (thisTry.length < 5) {
+              let shortMsg = "Too Short";
+              showModal(shortMsg, rowReset);
+              return;
+            } else {
+              checkWord();
+              return;
             }
-          }, delay);
 
-      } // end for-loop
-    } // VERIFY MAIN PART END
-    nextTry();
-
-      function nextTry() {
-        if (
-          thisTry.every((val, index) => val === answer[index])
-        ) {
-          youWin();
-          return;
-        } else if (remainingTries === 1) {
-          gameOver();
-        } else {
-          remainingTries--;
-          console.log(`${remainingTries} tries are left!`);
-          thisTry = [];
-          nextLetter = 0;
-        }
-      }
-  } // VERIFY END
-
-  function checkWord() {
-    const theWord = thisTry.join("").toLocaleLowerCase();
-    const wordResult = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${theWord}`);
-      wordResult.then( response => {
-        if (response) {
-          console.log(response.json());
-        } else {
-          console.log(`no response`);
-        }
+          } else {
+            if (thisTry.length >= 5) {
+              return;
+            }
+            placeLetter(keyTap);
+          }
+        });
       });
-  }
 
-  const modal = document.querySelector(".modal");
-  const shortModal = document.createElement("div");
-  shortModal.className = "shortModal";
-  shortModal.innerText = "Too Short";
-  const closer = document.createElement("button");
-  closer.classList.add("keyboard-button", "short-closer");
-  shortModal.appendChild(closer);
-  closer.innerText = "OK";
+      function placeLetter(entry) {
+        if (nextLetter > 4) {
+          return;
+        }
+        let row = totalTries - remainingTries;
+        let box = nextLetter;
+        document.querySelector(`#row${row}box${box}`).innerText = entry;
+        thisTry.push(entry);
+        nextLetter++;
+      }
 
-  function tooShort() {
-    modal.classList.add("flex");
-    modal.appendChild(shortModal);
-    closer.addEventListener("click", () => {
-      modal.removeChild(shortModal);
-      modal.classList.remove("flex");
-    });
-  }
+      function deleteOne() {
+        if (nextLetter <= 0) {
+          return;
+        }
+        let row = totalTries - remainingTries;
+        let box = nextLetter - 1;
+        document.querySelector(`#row${row}box${box}`).innerText = "";
+        nextLetter--;
+        thisTry.pop();
+      }
 
-  function youWin() {
-    modal.classList.add("flex");
-  }
+      function checkWord() {
+        const theWord = thisTry.join("").toLocaleLowerCase();
+        const wordPromise = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${theWord}`)
+          // .then(res => console.log(res.ok));
+          .then(res => {
+            if (res.ok) {
+              verify();
+            } else {
+              console.log("Not a Word");
+              let noWordMsg = "This word is not in our list";
+              showModal(noWordMsg, rowReset);
+            }
+          });
+      }
 
-  function gameOver() {
-    console.log("game over")
-  }
+      function verify() {
+        let answer = Array.from(wordleWord);
+        let row = totalTries - remainingTries
+        for (let place = 0; place < thisTry.length; place++) {
+          let box = document.querySelector(`#row${row}box${place}`);
+          let boxText = box.textContent;
+          let resultClass;
+          if (thisTry[place] === answer[place]) {
+            resultClass = "spot-on";
+          } else if (answer.includes(thisTry[place])) {
+              resultClass = "almost";
+            } else {
+              resultClass = "wrong";
+            }
+            let delay = 500 * place;
+            setTimeout(() => {
+              box.classList.add(resultClass);
+              for (const keyButton of document.querySelectorAll(".keyboard-button")) {
+                if (keyButton.innerText === boxText) {
+                  keyButton.classList.add(resultClass);
+                  break;
+                }
+              }
+            }, delay);
+          } // end for-loop
+          setTimeout(nextTry, 3000);
 
-})();
+          function nextTry() {
+            if (
+              thisTry.every((val, index) => val === answer[index])
+            ) {
+              let winMsg = "You Win. Play Again?"
+              showModal(winMsg, totalReset);
+              return;
+            } else if (remainingTries === 1) {
+              let loseMsg = "Sorry, no more tries.";
+              showModal(loseMsg, totalReset)
+            } else {
+              remainingTries--;
+              console.log(`${remainingTries} tries are left!`);
+              thisTry = [];
+              nextLetter = 0;
+            }
+          }
+        } // VERIFY END
+
+
+              function showModal(labelMessage, daFunc) {
+                modal.classList.add("flex");
+                const shortModal = document.querySelector(".inner-modal");
+                shortModal.classList.add("flex");
+                const modalLabel = document.querySelector(".inner-modal > h4");
+                modalLabel.innerText = labelMessage;
+                const closer = document.querySelector(".short-closer");
+                closer.addEventListener("click", () => {
+                  modalLabel.innerText = "";
+                  shortModal.classList.remove("flex");
+                  modal.classList.remove("flex");
+                  daFunc();
+
+
+
+                      });
+              }
+
+              function rowReset(){
+                let row = totalTries - remainingTries;
+                const boxes = document.querySelectorAll(`#row-${row} > .wordle-box`);
+                boxes.forEach( box => {
+                  box.innerText = "";
+                  nextLetter = 0;
+                  thisTry = [];
+                });
+              }
+
+              function totalReset() {
+                const boxes = document.querySelectorAll(".wordle-box");
+                boxes.forEach(box => {
+                  box.innerText = "";
+                  box.className = "wordle-box";
+                });
+                remainingTries = totalTries;
+                thisTry = [];
+                nextLetter = 0;
+                document.querySelectorAll(".keyboard-button").forEach(keyButton => {
+                  keyButton.className = "keyboard-button";
+                });
+                //
+                ////////////////////  NEW WORDLE WORD
+                //
+              }
+
+      }
+
+      guessWord();
+
+    })();
